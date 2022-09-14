2d670e969ae2023801ea1e57f813be26be088008
YERMIN YAIR LINO SANCHEZ <yermin.linos@ug.edu.ec>
2022-09-14 17:36:53 -0500

    DockerFuncional, aunque ustedes deben crear la imagen de todas formas. XD

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Console.WriteLine("***** Configurando Servicios *******");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -12,6 +14,10 @@ builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+Console.WriteLine("***** Finalizado configuración de servicios *******");
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,4 +33,27 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//Llamar el método para validación de conexión en BD
+var scope = app.Services.CreateScope();
+await Migrations(scope.ServiceProvider);
+
 app.Run();
+
+//Verificar la conexión con la base de datos
+async Task Migrations(IServiceProvider serviceProvider)
+{
+    var context = serviceProvider.GetService<AppDbContext>();
+    var conn_appdb = context.Database.GetDbConnection();
+
+    Console.WriteLine($"Conexion actual AppDB: {conn_appdb.ToString()} {Environment.NewLine} {conn_appdb.ConnectionString}");
+    Console.WriteLine("********** Probando acceso ******************");
+
+    try
+    {
+        Console.WriteLine("Base de datos disponible: " + context.Database.CanConnect()); ;
+    }
+    catch (Exception ex)
+    {
+        Console.Write($"Error al conectar: {ex.Message}");
+    }
+}
